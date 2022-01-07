cf40acbb737ba145fbd6e95e640e94ebafd7726f
JCWashington <jasmine.washington412@gmail.com>
2022-01-07 16:38:41 -0500

    cleaned up code by combining  the previous renderLicenseLink function with renderLicenseBadge

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,50 +1,45 @@
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  //https://img.shields.io/badge/<LABEL>-<MESSAGE>-<COLOR>
+  // This will render the badge (and it's link) for every license type
+  // ref: https://naereen.github.io/badges/
   //"GNU AGPLv3", "GNU GPLv3", "GNU LGPLv3", "Mozilla", "MIT", "Apache", "Boost"
   if (license === "GNU AGPLv3"){
-    return "https://img.shields.io/badge/license-AGPLv3-blue"
+    return "[![License: AGPL v3](https://img.shields.io/badge/license-AGPLv3-blue.svg)](https://opensource.org/licenses/AGPL-3.0)"
   } else if (license === "GNU GPLv3"){
-    return "https://img.shields.io/badge/license-GPLv3-blue"
+    return "[![License: GPL v3](https://img.shields.io/badge/license-GPLv3-blue.svg)](https://opensource.org/licenses/GPL-3.0)"
   } else if (license === "GNU LGPLv3") {
-    return "https://img.shields.io/badge/license-LGPLv3-blue"
+    return "[![License: LGPL v3](https://img.shields.io/badge/license-LGPLv3-blue.svg)](https://opensource.org/licenses/LGPL-3.0)"
   } else if (license === "Mozilla") {
-    return "https://img.shields.io/badge/license-Mozilla-brightgreen"
+    return "[![License: Mozilla](https://img.shields.io/badge/license-Mozilla-brightgreen)](https://opensource.org/licenses/MPL-2.0)"
   } else if (license === "MIT") {
-    return "https://img.shields.io/badge/license-MIT-green"
+    return "[![License: MIT](https://img.shields.io/badge/license-MIT-green)](https://opensource.org/licenses/MIT)"
   } else if (license === "Apache") {
-    return "https://img.shields.io/badge/license-Apache-yellow"
+    return "[![License: Apache](https://img.shields.io/badge/license-Apache-yellow)](https://opensource.org/licenses/Apache-2.0)"
   } else if (license === "Boost") {
-    return "https://img.shields.io/badge/license-Boost-orange"
+    return "[![License: Boost](https://img.shields.io/badge/license-Boost-orange)](https://opensource.org/licenses/BSL-1.0)"
   } else{
     return ' '
   }
 }
 // If there is no license, return an empty string
-function renderLicenseLink(license) {
-  if (license === "GNU AGPLv3"){
-    return "https://opensource.org/licenses/AGPL-3.0"
-  } else if (license === "GNU GPLv3"){
-    return "https://opensource.org/licenses/GPL-3.0"
-  } else if (license === "GNU LGPLv3") {
-    return "https://opensource.org/licenses/LGPL-3.0"
-  } else if (license === "Mozilla") {
-    return "https://opensource.org/licenses/MPL-2.0"
-  } else if (license === "MIT") {
-    return "https://opensource.org/licenses/MIT"
-  } else if (license === "Apache") {
-    return "https://opensource.org/licenses/Apache-2.0"
-  } else if (license === "Boost") {
-    return "https://opensource.org/licenses/BSL-1.0"
-  } else{
-    return ' '
-  }
-}
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {
-
-}
+// function renderLicenseLink(license) {
+//   if (license === "GNU AGPLv3"){
+//     return "https://opensource.org/licenses/AGPL-3.0"
+//   } else if (license === "GNU GPLv3"){
+//     return "https://opensource.org/licenses/GPL-3.0"
+//   } else if (license === "GNU LGPLv3") {
+//     return "https://opensource.org/licenses/LGPL-3.0"
+//   } else if (license === "Mozilla") {
+//     return "https://opensource.org/licenses/MPL-2.0"
+//   } else if (license === "MIT") {
+//     return "https://opensource.org/licenses/MIT"
+//   } else if (license === "Apache") {
+//     return "https://opensource.org/licenses/Apache-2.0"
+//   } else if (license === "Boost") {
+//     return "https://opensource.org/licenses/BSL-1.0"
+//   } else{
+//     return ' '
+//   }
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
